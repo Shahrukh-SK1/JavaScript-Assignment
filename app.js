17d502f1ea29d49b0a82a9282813256914c9fa91
shahrukh-SK1 <shahrukhayub6111@gmail.com>
2025-08-01 02:12:48 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+alert ("Thanks for your input")
+alert ("Welcome to my Website")
+
+
+var firstName = "Shahrukh"
+var lastName = "Ayub"
+var fullName = firstName + " " + lastName;
+alert (fullName);  
+
+
+var num = 25;
+var newNum = num + 5;
+alert (newNum)
