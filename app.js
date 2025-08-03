c0a68178ee53f6ebbb9a9f4688f86a7a5915b1f5
Shahrukh-SK1 <shahrukhayub6111@gmail.com>
2025-08-04 02:16:36 +0500

    Complete Assignment

--- app.js
+++ app.js
@@ -1,13 +1,201 @@
-alert ("Thanks for your input")
-alert ("Welcome to my Website")
+// Chapter No. 1
+
+// Answer No. 1
+
+alert ("Welcome to my Website");
+
+// Answer No. 2
+
+alert("Error! Please enter a valid password.");
+
+// Answer No. 3
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// Answer No. 4
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+
+// Answer No. 5
+
+alert("\nHello... I can run JS through my web browser’s console");
 
 
-var firstName = "Shahrukh"
-var lastName = "Ayub"
-var fullName = firstName + " " + lastName;
-alert (fullName);  
+// Chapter No. 2
+
+// Answer No. 1
+
+var username;
+
+// Answer No. 2
+ 
+var myName = "Muhammad Shahrukh"
+
+// Answer No. 3
+
+// a) Declare a variable
+var message;
+
+// b) Assign a string
+message = "Hello World";
+
+// c) Show in alert
+alert(message);
+
+// Answer No. 4
+
+var studentName = "Muhammad Shahrukh";
+var studentAge = "30 years old";
+var studentClass = "Web and App Development";
+
+// Show alerts
+alert(studentName);
+alert(studentAge);
+alert(studentClass);
 
 
-var num = 25;
-var newNum = num + 5;
-alert (newNum)
+// Answer No. 5
+
+var pattern = "Pizza\nPizz\nPiz\nPi\nP";
+alert(pattern);
+
+
+// Answer No. 6
+
+var email = "sharhukhayub6111@gmail.com";
+alert("My email address is " + email);
+
+// Answer No. 7
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the book " + book);
+
+
+
+
+
+// var firstName = "Shahrukh"
+// var lastName = "Ayub"
+// var fullName = firstName + " " + lastName;
+// alert (fullName);  
+
+// var nationality;
+// nationality = "Pakistan";
+
+// alert (nationality);
+
+// var thanks;
+// thanks = "Thanks for your input";
+
+// alert (thanks)
+
+// // Chapter No. 3
+
+// var num = 25;
+// var newNum = num + 5;
+// alert (newNum)
+
+// // Chapter No. 4
+
+// var userResponse;
+// userResponse = "Good";
+
+// alert (userResponse)
+
+
+
+// // Chapter No. 5
+
+// var popularNumber = 4;
+// var popularNumber = 2 + 2;
+
+// alert (2 + 2)
+
+// var number = 12;
+// var anotherNumber = 6;
+// var popularNumber = number + anotherNumber;
+
+// alert(popularNumber)
+
+// // Answer No 4
+
+// var num = 20;
+
+// alert(num % 6);
+
+// // Answer No 5
+
+// var largNum = 1000;
+
+// alert(largNum * 2000);
+
+// // Answer No 7
+
+// var doubledNum = 12 + 12;
+// alert(doubledNum);
+
+
+// // Answer No 10
+
+// var price = 100;
+// var qty = 10;
+
+// alert(price * qty);
+
+
+// // Answer No 10
+
+// var leftOver = 10 % 3;
+
+// alert(leftOver);
+
+
+// resultOfComputation = ((2 * 4) * 4) + 2;
+
+// alert(resultOfComputation);
+
+// // Answer No 15
+
+// var cost = 80;
+
+// alert(price - cost);
+
+
+
+// // Chapter No. 6
+
+
+// // Answer No 
+
+// var x = 50;
+// var y = --x;
+
+// alert(--x);
+
+// // Answer No 7
+
+// var newNum = 10;
+
+// // Answer No 10
+
+// var num = 5;
+// var newNum = --num;
+// console.log(newNum);
+
+// alert(newNum);
+
+// // Answer No 11
+
+// var num = 3;
+// var newNum = num++;
+// console.log(newNum);
+// alert(newNum++);
+
+
+
+
+
+
+
+
